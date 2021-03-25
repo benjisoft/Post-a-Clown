dd9130924c742c8cd5e967dfb2b298f5dccaead0
EC2 Default User <ec2-user@ip-10-0-0-240.eu-west-2.compute.internal>
2021-03-25 17:40:29 +0000

    Not good

--- submit.php
+++ submit.php
@@ -1,5 +1,10 @@
 <?php
 
-if (isset($_POST['submit'])) {
+export GOOGLE_APPLICATION_CREDENTIALS="config.json"
+
+if (isset($_POST['line1'])) {
     echo $_POST['line1'];
+    
+} else {
+    echo "I borked it!";
 }
